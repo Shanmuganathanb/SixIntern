09498f1b422081c8a3c98ef19bffe141a1f86893
SATYAM SHARMA <45912511+satyams2812@users.noreply.github.com>
2020-10-14 16:56:05 +0530

    v1

---
+++ controllers/admincontroller.js
@@ -0,0 +1,8 @@
+const user= require('./models/user');
+const user= require('./models/employee');
+
+
+exports.home = function(req, res) {
+    res.render('admin')
+  }
+
---
+++ controllers/usercontroller.js
@@ -0,0 +1,11 @@
+exports.home = function(req, res) {
+    res.render('index')
+  }
+
+  exports.login = function(req,res){
+     
+  }
+
+  //google login api
+
+  //first,email
